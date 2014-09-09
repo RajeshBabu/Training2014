13bcca357f4f1da37d32ebd77faa8ba3a750e2da
RajeshBabuPola <rajeshmca38@gmail.com>
2014-09-09 11:14:45 +0530

    Completed

--- SendCustomObj/src/com/pcs/sendcustomobj/SecondActivity.java
+++ SendCustomObj/src/com/pcs/sendcustomobj/SecondActivity.java
@@ -2,11 +2,21 @@ package com.pcs.sendcustomobj;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.pcs.helpers.Constants;
+import com.pcs.model.User;
 
 public class SecondActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+		TextView textView = (TextView)findViewById(R.id.message_txt);
+		User user = getIntent().getParcelableExtra(Constants.IntentExtras.USER_INFO);
+		if(user != null){
+			textView.setText(user.toString());
+		}
 	}
 
 }
